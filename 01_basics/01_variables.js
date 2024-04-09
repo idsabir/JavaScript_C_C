a845159fc6c335a4180b1cb2277f15807b61d0e4
Sabir Ansari <146080136+idsabir@users.noreply.github.com>
2024-04-09 18:48:16 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 23452
+let accountEmail ="ideal07@gmail.com"
+var accountPassword ="91723"
+let accountState;
+
+accountCity = "bihar"
+
+// accountId =2 // not allowed
+
+// curly bresses {} is called scope.
+
+
+ /*
+ prefer not to use var 
+ because of issue i block scope and function scope
+ */
+
+accountEmail ="idea07@gmail.com"
+ accountPassword ="6233"
+ accountCity = "Siwan"
+
+ console.table ([accountEmail,accountId,accountCity,accountPassword,accountState])
+console.log(accountId);
