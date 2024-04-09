8c9862b12eb8b6b758bce171aa38fd3f0de1e12e
Sabir Ansari <146080136+idsabir@users.noreply.github.com>
2024-04-09 19:19:34 +0000

    dataType

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)     // we are using nodejs, not browser
+console.log(3 + 3);
+console.log("Sabir");
+let name = "sabir"
+let age = 21
+let isLoggedIn = false
+
+
+// number => 2 to Power 53
+// bigint it is used to big number. ex - stock market, tradding, big website ,facebook etc.
+// string => ""
+// boolean => true/false
+// null => standalone value  null is a object
+// unfined => it is not define the value 
+// symbol => unique
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
